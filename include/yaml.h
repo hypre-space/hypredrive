e985cb93bae2ff1f36e5a761277c7e8628765198
Victor A. P. Magri <paludettomag1@llnl.gov>
2023-09-29 09:13:05 -0400

    Remove old YAML macros

--- include/yaml.h
+++ include/yaml.h
@@ -167,38 +167,4 @@ char* YAMLnodeFindChildValueByKey(YAMLnode*, const char*);
          (void*) _node->next); \
     } while(0)
 
-// TODO: Remove the following macros:
-
-
-#define YAML_CALL_IF_OPEN() if (0) {}
-#define YAML_CALL_IF_CLOSE(_node) else {}
-#define YAML_SET_IF_OPEN() if (0) {}
-#define YAML_SET_IF_CLOSE(_node) else { ErrorMsgAddUnknownKey(_node->key); }
-#define YAML_SET_IF_CLOSE_(_node) else { ErrorMsgAddInvalidKeyValPair(_node->key, _node->val); }
-#define YAML_SET_INTEGER_IF_KEY_MATCHES(_var, _key, _node) \
-   else if (!strcmp(_node->key, _key)) { _var = atoi(_node->val); }
-#define YAML_SET_INTARRAY_IF_KEY_MATCHES(_count, _array, _key, _node) \
-   else if (!strcmp(_node->key, _key)) { StrToIntArray(_node->val, _count, _array); }
-#define YAML_SET_REAL_IF_KEY_MATCHES(_var, _key, _node) \
-   else if (!strcmp(_node->key, _key)) { _var = atof(_node->val); }
-#define YAML_SET_STRING_IF_KEY_MATCHES(_var, _key, _node) \
-   else if (!strcmp(_node->key, _key)) { _var = strdup(_node->val); }
-#define YAML_SET_INTEGER_IF_VAL_MATCHES(_var, _val, _node) \
-   else if (!strcmp(_node->val, _val)) { _var = atoi(_node->val); }
-#define YAML_SET_INTEGER_IF_VAL_MATCHES_TWO(_var, _val0, _val1, _node) \
-   else if (!strcmp(_node->val, _val0) || !strcmp(_node->val, #_val1)) { _var = _val1; }
-#define YAML_SET_INTEGER_IF_VAL_MATCHES_ANYTWO(_var, _val0, _val1, _node) \
-   else if (!strcmp(_node->val, _val0) || !strcmp(_node->val, #_val1)) { _var = _val1; }
-#define YAML_SET_INTEGER_IF_KEY_EXISTS(_var, _key, _node) \
-   {char *val = YAMLnodeFindChildValueByKey(_node, _key); if (val != NULL) { _var = atoi(val); }}
-#define YAML_CALL_IF_KEY_MATCHES(_call, _args, _node, _key) \
-   else if (!strcmp(_node->key, _key)) { _call(_args, _node); }
-#define YAML_CALL_IF_VAL_MATCHES(_call, _args, _node, _val) \
-   else if (!strcmp(_node->val, _val)) { _call(_args, _node); }
-#define YAML_RETURN_IF_VAL_MATCHES(_node, _val) \
-   if (!strcmp(_node->val, _val)) { return; }
-#define YAML_INC_INTEGER_IF_KEY_EXISTS(_var, _key, p) \
-   { YAMLnode* c = p->children; while (c) { if (!strcmp(c->key, _key)) { _var++; } c = c->next; } }
-
-
 #endif
