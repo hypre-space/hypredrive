cc4e287c2d03937d05669f0455ac3ae75e179fff
Victor A. P. Magri <paludettomag1@llnl.gov>
2025-11-20 11:31:55 -0500

    Add laplacian and elasticity tests

--- cmake/HYPREDRV_Testing.cmake
+++ cmake/HYPREDRV_Testing.cmake
@@ -5,7 +5,9 @@
 
 # Function for adding tests
 function(add_hypredrive_test test_name num_procs config_file)
-    add_test(NAME ${test_name}
+    # Automatically prepend "hypredrive_test_" to the test name
+    set(full_test_name "hypredrive_test_${test_name}")
+    add_test(NAME ${full_test_name}
         COMMAND ${CMAKE_COMMAND}
                 -DLAUNCH_DIR=${CMAKE_SOURCE_DIR}
                 -DTARGET_BIN=$<TARGET_FILE:hypredrive>
@@ -19,11 +21,50 @@ function(add_hypredrive_test test_name num_procs config_file)
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     )
 
-    set_tests_properties(${test_name}
+    set_tests_properties(${full_test_name}
         PROPERTIES
         FAIL_REGULAR_EXPRESSION "HYPREDRIVE Failure!!!|Abort|Error|failure"
         SKIP_REGULAR_EXPRESSION "\\[test\\] Skipping example:"
     )
+endfunction()
+
+# Function for adding tests for standalone executable drivers
+function(add_executable_test test_name target num_procs)
+    cmake_parse_arguments(EXEC_TEST
+        "RUN_SERIAL"
+        "FAIL_REGULAR_EXPRESSION;WORKING_DIRECTORY"
+        "ARGS"
+        ${ARGN}
+    )
+
+    # Default values
+    if(NOT DEFINED EXEC_TEST_FAIL_REGULAR_EXPRESSION)
+        set(EXEC_TEST_FAIL_REGULAR_EXPRESSION "HYPREDRIVE Failure!!!|Abort|Error|failure")
+    endif()
+    if(NOT DEFINED EXEC_TEST_WORKING_DIRECTORY)
+        set(EXEC_TEST_WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+    endif()
+
+    add_test(NAME ${test_name}
+        COMMAND ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${num_procs}
+                ${MPIEXEC_PREFLAGS}
+                $<TARGET_FILE:${target}>
+                ${EXEC_TEST_ARGS}
+                ${MPIEXEC_POSTFLAGS}
+        WORKING_DIRECTORY ${EXEC_TEST_WORKING_DIRECTORY}
+    )
+
+    set_tests_properties(${test_name}
+        PROPERTIES
+            FAIL_REGULAR_EXPRESSION "${EXEC_TEST_FAIL_REGULAR_EXPRESSION}"
+    )
+
+    if(EXEC_TEST_RUN_SERIAL)
+        set_tests_properties(${test_name}
+            PROPERTIES
+                RUN_SERIAL TRUE
+        )
+    endif()
 endfunction()
 
 # Function for adding tests with output verification
@@ -60,7 +101,9 @@ function(add_hypredrive_test_with_output test_name num_procs config_file example
     endif()
 endfunction()
 
-if(HYPREDRV_ENABLE_TESTING)
+# Only register tests when included from the main CMakeLists.txt
+# (not when included from subdirectories like examples)
+if(HYPREDRV_ENABLE_TESTING AND CMAKE_CURRENT_SOURCE_DIR STREQUAL CMAKE_SOURCE_DIR)
     # Must be called before add_subdirectory(tests) so that add_test() calls work
     enable_testing()
 
@@ -68,17 +111,17 @@ if(HYPREDRV_ENABLE_TESTING)
     add_subdirectory(tests)
 
     # Add tests
-    add_hypredrive_test(test_ex1_1proc  1 ex1.yml)
-    add_hypredrive_test(test_ex1a_1proc 1 ex1a.yml)
-    add_hypredrive_test(test_ex1b_1proc 1 ex1b.yml)
-    add_hypredrive_test(test_ex1c_1proc 1 ex1c.yml)
-    add_hypredrive_test(test_ex1d_1proc 1 ex1d.yml)
-    add_hypredrive_test(test_ex2_4proc  4 ex2.yml)
-    add_hypredrive_test(test_ex3_1proc  1 ex3.yml)
-    add_hypredrive_test(test_ex4_4proc  4 ex4.yml)
-    add_hypredrive_test(test_ex5_1proc  1 ex5.yml)
+    add_hypredrive_test(ex1_1proc  1 ex1.yml)
+    add_hypredrive_test(ex1a_1proc 1 ex1a.yml)
+    add_hypredrive_test(ex1b_1proc 1 ex1b.yml)
+    add_hypredrive_test(ex1c_1proc 1 ex1c.yml)
+    add_hypredrive_test(ex1d_1proc 1 ex1d.yml)
+    add_hypredrive_test(ex2_4proc  4 ex2.yml)
+    add_hypredrive_test(ex3_1proc  1 ex3.yml)
+    add_hypredrive_test(ex4_4proc  4 ex4.yml)
+    add_hypredrive_test(ex5_1proc  1 ex5.yml)
     if (HYPREDRV_ENABLE_EIGSPEC)
-        add_hypredrive_test(test_ex6_1proc 1 ex6.yml)
+        add_hypredrive_test(ex6_1proc 1 ex6.yml)
     endif()
-    add_hypredrive_test(test_ex7_1proc  1 ex7.yml)
+    add_hypredrive_test(ex7_1proc  1 ex7.yml)
 endif()
